e1c6f94118446c070e6405579a9233fc0ca370ca
Jason Hao <jwh259@gmail.com>
2014-07-09 16:01:52 -0400

    Update note2_calculateAngle.js
    changed the angle value to be between 0 to 180

--- note2_calculateAngle.js
+++ note2_calculateAngle.js
@@ -2,26 +2,37 @@
     var time = new Date();
 //    var h = time.getHours;
     var n = time.toLocaleTimeString();
-//    document.write(n);
-    var h = n.substring(0,2);
-    document.write(h);
-//    alert(time);
-//    alert(h);
-    var m = n.substring(3,5);
-    document.write(m);
-//    alert(m);
-    
-    var hour = 11;
-    var minute = 59;
-    var jiao1 = 30* hour + minute/120;   //change hour and minute to h and m as above
-    var jiao2 = 6*minute;                //then we can calculate the time right now
-    var jiao;
-    if (jiao2 - jiao1 >= 0){
-    jiao= jiao2 - jiao1;
+    if (n[1]=":"){
+    var hour = n.substring(0,1);
+    var minute = n.substring(2,4);  
     }
-    else jiao = jiao1 - jiao2;
+    else
+    {
+    var hour = n.substring(0,2);
+    var minute = n.substring(3,5);
+    }
     
-    document.write("<br/>"+jiao);
-//    document.write(time);
+//    var hour =3;   //change the time manually, hour shoule be between 0 to 12, minute 0 to 60
+//    var minute =57;
+    
+    var angle1 = 30 * hour + minute/120;   //change hour and minute to h and m as above
+    var angle2 = 6 * minute;                //then we can calculate the time right now
+    var angle;
+    if ((angle2 - angle1 >= 0) && (angle2 - angle1 <= 360)){
+    angle= angle2 - angle1;
+    }
+    else {
+    angle = angle1 - angle2;
+        
+    }
+    if (angle > 180){
+    angle = 360 - angle;
+    }
+    
+    document.write("The time is: "+n+"<br/>");
+    document.write("The FULL time is: "+time+"<br/>");
+    document.write("The hour is: "+hour+"<br/>");
+    document.write("The minute is: "+minute+"<br/>");
+    document.write("The angle is: "+angle+" degree");
     
 </script>
